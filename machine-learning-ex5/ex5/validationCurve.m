78268cf0b6b05dd4bc37f3557fc1fb004e30023e
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-05-16 23:54:37 +0900

    Complete ex5

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -17,30 +17,47 @@ error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the validation errors in error_val. The 
-%               vector lambda_vec contains the different lambda parameters 
-%               to use for each calculation of the errors, i.e, 
-%               error_train(i), and error_val(i) should give 
-%               you the errors obtained after training with 
+% Instructions: Fill in this function to return training errors in
+%               error_train and the validation errors in error_val. The
+%               vector lambda_vec contains the different lambda parameters
+%               to use for each calculation of the errors, i.e,
+%               error_train(i), and error_val(i) should give
+%               you the errors obtained after training with
 %               lambda = lambda_vec(i)
 %
 % Note: You can loop over lambda_vec with the following:
 %
 %       for i = 1:length(lambda_vec)
 %           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
+%           % Compute train / val errors when training linear
 %           % regression with regularization parameter lambda
 %           % You should store the result in error_train(i)
 %           % and error_val(i)
 %           ....
-%           
+%
 %       end
 %
 %
 
 
+m = size(X, 1);
 
+for i = 1:m
+	for j = 1:length(lambda_vec)
+		lambda = lambda_vec(j);
+
+		Xtrain = X(1:i,:);
+		ytrain = y(1:i);
+
+		theta = trainLinearReg(Xtrain, ytrain, lambda);
+
+		[Jtrain,grad_train]=linearRegCostFunction(Xtrain,ytrain,theta,0);
+		[Jval,grad_val]=linearRegCostFunction(Xval,yval,theta,0);
+
+		error_train(j) = Jtrain;
+	    error_val(j) = Jval;
+	end
+end
 
 
 

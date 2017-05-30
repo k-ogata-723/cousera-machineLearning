a390a04ef6b6cea17bd8c334fa7ba098ac3beba3
KOKI OGATA <ogata.koki.0723@gmail.com>
2017-05-30 23:26:11 +0900

    Complete ex6 1.2.3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,18 +15,35 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+results = eye(64, 3);
+errorRow = 0;
 
+% Cs = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+% sigmas = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+for Cs = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+  for sigmas = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    errorRow = errorRow + 1;
+    model = svmTrain(X, y, Cs,  @(x1, x2) gaussianKernel(x1, x2, sigmas));
+    predictions = svmPredict(model, Xval);
+    prediction_error = mean(double(predictions ~= yval));
 
+    results(errorRow, :) = [Cs, sigmas, prediction_error];
+  end
+end
 
+sorted_results = sortrows(results, 3);
+
+C = sorted_results(1,1);
+simga = sorted_results(1,2);
 
 
 % =========================================================================
